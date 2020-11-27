e4467d0ea1746173013f3ff8e3b8e8a61ae7ccb8
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-11-27 16:05:54 -0500

    Update food.py

--- food.py
+++ food.py
@@ -2,10 +2,13 @@
 actionlist = {
     'webapi_a_thing':'file_that_this_action_requires.php'
 } # that's a placeholder lul
-from flask import request, Flask
+from flask import request, Flask, send_from_directory
 app = Flask(__app__)
 @app.route('/nwapi.php')
 def api():
-     print(action_list[request.args[action]])
-     return ''
+     try:
+        print(action_list[request.args[action]])
+        return ''
+     except:
+        return send_from_directory('.','aio.php')
  
