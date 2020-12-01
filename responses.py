259881811822329436cc11bffccbe767311124cb
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-12-01 15:38:26 -0500

    Update responses.py

--- responses.py
+++ responses.py
@@ -101,7 +101,7 @@ def category_list(request):
 
     return {
         # Must be 食事 and encoded in Shift-JIS.
-        "LargeCategoryName": CDATA(Kana("ピズ")),
+        "LargeCategoryName": CDATA("ピズ"),
         "CategoryList": {
             "CategoryCode": "1",
             "ShopList": {
