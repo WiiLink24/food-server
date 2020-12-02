097274cbf20f65781e555840194c82b1ffa285f1
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-12-02 00:07:39 -0500

    Update responses.py

--- responses.py
+++ responses.py
@@ -101,7 +101,7 @@ def category_list(request):
 
     return {
         # Must be 食事 and encoded in Shift-JIS.
-        "LargeCategoryName": Kana("ピズ"),
+        "yomiLargeCategoryName": "食事",
         "CategoryList": {
             "CategoryCode": "1",
             "ShopList": {
