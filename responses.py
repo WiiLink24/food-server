a83575ac50fef2281d6d6baaa8fc67666b54cead
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-12-13 12:15:09 -0500

    let's hope this works (2)

--- responses.py
+++ responses.py
@@ -103,23 +103,25 @@ def category_list(request):
         # Must be 食事 and encoded in Shift-JIS.
         "LargeCategoryName":"meal",
         "CategoryList": {
-            "CategoryCode": "1",
-            "ShopList": {
-                "TheShopShop":{
-                    "shopCode":1,
-                    "homeCode":1,
-                    "name":"You wanted a name?",
-                    "catchphrase":"on",
-                    "minPrice":1,
-                    "yoyaku":1,
-                    "activate":1,
-                    "waitTime":1,
-                    "paymentList":{
-                        "athing":"代金引換" 
-                    },
-                    "shopStatus":{
-                        "status":{
-                            "isOpen":"1",
+            "ACategory":
+                "CategoryCode": "1",
+                "ShopList": {
+                    "TheShopShop":{
+                        "shopCode":1,
+                        "homeCode":1,
+                        "name":"You wanted a name?",
+                        "catchphrase":"on",
+                        "minPrice":1,
+                        "yoyaku":1,
+                        "activate":1,
+                        "waitTime":1,
+                        "paymentList":{
+                            "athing":"代金引換" 
+                        },
+                        "shopStatus":{
+                            "status":{
+                                "isOpen":"1",
+                            }
                         }
                     }
                 }
