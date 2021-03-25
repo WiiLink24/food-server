fa71457eda7f3a14b29f95c7d1a51656993cb821
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-03-25 14:14:25 -0400

    Add shop_code

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ class Shops(db.Model):
     name = db.Column(db.String, nullable=False, primary_key=True)
     category_code = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String, nullable=False)
+    shop_code = db.Column(db.Integer, nullable=False)
     restaurant_id = db.Column(db.Integer, nullable=False)
     wait_time = db.Column(db.Integer, nullable=False)
     open = db.Column(db.Boolean, nullable=False)
