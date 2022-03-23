39350da181d7f54120ac3c76ec9ab8f28a0a3b59
Spotlight <spotlight@joscomputing.space>
2022-03-23 15:26:14 -0500

    Ensure images directory exists

--- thepantry/encodemii.py
+++ thepantry/encodemii.py
@@ -1,8 +1,16 @@
 import io
+import os
+
 from PIL import Image, ImageFile
 
 
+def ensure_images_exists():
+    os.makedirs("./images", exist_ok=True)
+
+
 def save_restaurant_logo(in_bytes: bytes, restaurant_id: int):
+    ensure_images_exists()
+
     logo_data = generic_encode(in_bytes, 144, 144)
     logo = open(f"./images/{restaurant_id}.jpg", "wb")
     logo.write(logo_data)
@@ -10,6 +18,8 @@ def save_restaurant_logo(in_bytes: bytes, restaurant_id: int):
 
 
 def save_food_image(in_bytes: bytes, restaurant_id: int, item_code: int):
+    ensure_images_exists()
+
     image_data = generic_encode(in_bytes, 160, 160)
     image = open(f"./images/{restaurant_id}/{item_code}.jpg", "wb")
     image.write(image_data)
