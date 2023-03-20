0645badde876add23c2e63990f935174dfc5950c
Sketch <75850871+noahpistilli@users.noreply.github.com>
2023-03-20 12:31:46 -0400

    Money is a float

--- responses.py
+++ responses.py
@@ -74,7 +74,7 @@ def basket_list(request):
     data = []
 
     for i in range(num):
-        price = int(query.basket[i]["price"].replace("$", "")) * int(query.basket[i]["qty"]) + price
+        price = float(query.basket[i]["price"].replace("$", "")) * float(query.basket[i]["qty"]) + float(price)
         data.append(
             RepeatedElement(
                 {
